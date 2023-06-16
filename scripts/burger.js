2d8b0a4079fdf0ee0f47956b57fa71353c031d12
cou23001 <cou23001@byui.edu>
2023-06-16 06:57:56 -0700

    nigth update

--- scripts/burger.js
+++ scripts/burger.js
@@ -10,6 +10,12 @@ const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 const cards = document.querySelectorAll(".card"); 
 const bullets = document.querySelector(".bullets");
+const body = document.querySelector("body");
+const bulletsa = document.querySelectorAll(".bullets a");
+const alerts = document.querySelectorAll("alerts");
+const ps = document.querySelectorAll("p");
+const ullia = document.querySelectorAll("ul li a");
+const footer = document.querySelector("footer");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("☑️")) {
@@ -18,15 +24,39 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "❎";
     cards.forEach((card) => {
       card.style.background = "#001c3d"; 
+      card.style.color = "#fff"; 
     });
-    bullets.style.background = "#fff";
+    bulletsa.forEach((bullet) => {
+      bullet.style.color ="#fff"
+    });
+    ps.forEach((p) => {
+      p.style.color ="#fff"
+    });
+    bullets.style.background = "#001C3D";
+    body.style.background = "#fff"
+    bullets.style.color = "#fff"
+    footer.style.background = "#001C3D";
 	} else {
-		main.style.background = "#eee";
+		main.style.background = "#FFFFFF";
 		main.style.color = "#000";
 		modeButton.textContent = "☑️";
     cards.forEach((card) => {
       card.style.background = "#f0f0f0";
     });
-    bullets.style.background = "#fff";
+    ullia.forEach((bullet) => {
+      bullet.style.color ="#fff"
+    });
+    bulletsa.forEach((bullet) => {
+      bullet.style.color ="#000"
+    });
+    
+    bullets.style.background = "#f0f0f0";
+    body.style.background = "#fff";
+    ps.forEach((p) => {
+      p.style.color = "#000";
+    });
+    
+    footer.style.background = "#f0f0f0";
+    bullets.style.color = "#000"
 	}
 });

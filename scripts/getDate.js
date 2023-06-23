ae634407abcc67ff44720745ae9545af759b8078
cou23001 <cou23001@byui.edu>
2023-06-23 01:25:50 -0700

    hero update

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -7,3 +7,17 @@ currentYearSpan.innerHTML = new Date().getFullYear();
 const lastModifiedParagraph = document.getElementById('lastModified');
 //lastModifiedParagraph.textContent = 'Last modified: ' + document.lastModified;
 lastModifiedParagraph.innerHTML = 'Last modified: ' + document.lastModified;
+
+
+// current visits
+var currentVisits = localStorage.getItem('visits');
+
+if (currentVisits === null || currentVisits === undefined) {
+  currentVisits = 1;
+} else {
+  currentVisits = parseInt(currentVisits, 10) + 1;
+}
+
+localStorage.setItem('visits', currentVisits);
+document.getElementById('visits').textContent = currentVisits;
+

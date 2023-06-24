d89a023f0f1128dfb159729cac2583a9b0275ba4
cou23001 <cou23001@byui.edu>
2023-06-24 10:01:49 -0700

    The Chamber Discover

--- chamber/scripts/getDate.js
+++ chamber/scripts/getDate.js
@@ -6,3 +6,31 @@ currentYearSpan.innerHTML = new Date().getFullYear();
 const lastModifiedParagraph = document.getElementById('lastModified');
 lastModifiedParagraph.textContent = 'Last modified: ' + document.lastModified;
 //lastModifiedParagraph.innerHTML = 'Last modified: ' + document.lastModified;
+
+flatpickr('#calendar', {
+    inline: true
+});
+
+// Print the messages in the sidebar
+var currentDate = new Date();
+
+var previousVisit = localStorage.getItem('visitDate');
+
+if (previousVisit) {
+  var timeDiff = currentDate - new Date(previousVisit);
+
+  var daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+  if (daysDiff === 0) {
+    document.getElementById('message').textContent = "Back so soon! Awesome!";
+  } else {
+    var message = "You last visited " + daysDiff + " ";
+    message += daysDiff === 1 ? "day" : "days";
+    message += " ago.";
+    document.getElementById('message').textContent = message;
+  }
+} else {
+  document.getElementById('message').textContent = "Welcome! Let us know if you have any questions.";
+}
+
+localStorage.setItem('visitDate', currentDate);

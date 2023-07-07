e62fddd61b1d1b1764a0994e02b686d31df56ff5
cou23001 <cou23001@byui.edu>
2023-07-06 23:14:19 -0700

    data json

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,37 @@
-const baseURL = "https://yourgithubusername.github.io/wdd230/";
-
+const baseURL = "https://cou23001.github.io/wdd230/";
 const linksURL = "https://cou23001.github.io/wdd230/data/links.json";
+const currentWeek = document.querySelector('#current-week');
+
+async function apiFetch() {
+    try {
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}   
+
+function displayResults(data) {
+    const displayWeeks = (weeks) => {
+        weeks.forEach((week) => {
+            currentWeek.innerHTML = ``;
+            /*
+            currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+            const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+            let desc = data.weather[0].description;
+            //console.log(desc);
+            weatherIcon.setAttribute('src',iconsrc);
+            weatherIcon.setAttribute('alt',desc);
+            captionDesc.textContent = `${desc}`;
+            */
+        });
+    }
+}
+apiFetch();

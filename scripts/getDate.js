ee3a51cad6cc4d792bcdb3a9f1baab97259c9706
cou23001 <cou23001@byui.edu>
2023-07-06 22:56:37 -0700

    Dynamic Weather

--- scripts/getDate.js
+++ scripts/getDate.js
@@ -22,3 +22,38 @@ document.addEventListener("DOMContentLoaded", function() {
   localStorage.setItem('visits', currentVisits);
   document.getElementById('visits').textContent = currentVisits;
 });
+
+const currentTemp = document.querySelector('#weather');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=16.75&lon=-93.1167&appid=4a8dfd912f7300f5b84fbba2ec797ee8&units=imperial';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}   
+
+function displayResults(data) {
+    let desc = data.weather[0].description;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    currentTemp.innerHTML = `${data.main.temp}&deg;F - ${desc}`;
+    
+    
+    //console.log(desc);
+    weatherIcon.setAttribute('src',iconsrc);
+    weatherIcon.setAttribute('alt',desc);
+    captionDesc.textContent = `${desc}`;
+}
+apiFetch();
